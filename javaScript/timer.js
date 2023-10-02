1c41e2e57cbaa90a564be0b0d35aded452cd1af5
Grace <yintingchancytg@gmail.com>
2023-10-02 14:54:05 +0200

    Created numeric timer

--- javaScript/timer.js
+++ javaScript/timer.js
@@ -1,7 +1,6 @@
 "use strict";
 
 function renderTimer(parameters) {
-  console.log(parameters);
   let navigationHeader = document.querySelector(".navigationHeader");
   navigationHeader.innerHTML = `
     <img src="./images/arrow.png" alt="arrow" class="arrow">
@@ -16,4 +15,30 @@ function renderTimer(parameters) {
       <button class="button-reset">Reset</button>
     </div>
   `;
+
+  const numericTimer = document.querySelector(".numeric-timer");
+  numericTimer.textContent = "00:00";
+  updateNumericTimer(numericTimer, parameters);
+}
+
+function updateNumericTimer(numericTimer, parameters) {
+  if (parameters.cookingDirection === "Soft" && parameters.size === "Small") {
+    numericTimer.textContent = "06:00";
+  } else if (parameters.cookingDirection === "Soft" && parameters.size === "Medium") {
+    numericTimer.textContent = "07:00";
+  } else if (parameters.cookingDirection === "Soft" && parameters.size === "Large") {
+    numericTimer.textContent = "08:00";
+  } else if (parameters.cookingDirection === "Medium" && parameters.size === "Small") {
+    numericTimer.textContent = "08:00";
+  } else if (parameters.cookingDirection === "Medium" && parameters.size === "Medium") {
+    numericTimer.textContent = "09:00";
+  } else if (parameters.cookingDirection === "Medium" && parameters.size === "Large") {
+    numericTimer.textContent = "10:00";
+  } else if (parameters.cookingDirection === "Hard" && parameters.size === "Small") {
+    numericTimer.textContent = "10:00";
+  } else if (parameters.cookingDirection === "Hard" && parameters.size === "Medium") {
+    numericTimer.textContent = "11:00";
+  } else if (parameters.cookingDirection === "Hard" && parameters.size === "Large") {
+    numericTimer.textContent = "12:00";
+  }
 }

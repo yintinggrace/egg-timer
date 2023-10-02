ed6aede200a8b1055e9bb0e09f9bf898325b0e72
Grace <yintingchancytg@gmail.com>
2023-10-02 14:20:46 +0200

    Created buttons

--- javaScript/timer.js
+++ javaScript/timer.js
@@ -6,4 +6,14 @@ function renderTimer(parameters) {
   navigationHeader.innerHTML = `
     <img src="./images/arrow.png" alt="arrow" class="arrow">
   `;
+
+  let wrapper = document.querySelector(".wrapper");
+  wrapper.innerHTML = `
+    <div class="visual-timer"></div>
+    <div class="numeric-timer">xxx</div>
+    <div class="button-timer">
+      <button class="button-start">Start</button>
+      <button class="button-reset">Reset</button>
+    </div>
+  `;
 }

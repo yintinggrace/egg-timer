01127866e8560b25f0291476bf7a633282957e20
Grace <yintingchancytg@gmail.com>
2023-10-02 13:56:19 +0200

    Added an arrow

--- javaScript/timer.js
+++ javaScript/timer.js
@@ -2,4 +2,8 @@
 
 function renderTimer(parameters) {
   console.log(parameters);
+  let navigationHeader = document.querySelector(".navigationHeader");
+  navigationHeader.innerHTML = `
+    <img src="./images/arrow.png" alt="arrow" class="arrow">
+  `;
 }

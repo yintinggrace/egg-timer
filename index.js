9fa47c59ec78d6d65fe8454fbca31bc054606ac1
Grace <yintingchancytg@gmail.com>
2023-10-03 19:15:47 +0200

    Updated the app logo

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 "use strict";
-renderStep1();
-/*
+
 const headerDom = document.querySelector("header");
 const mainDom = document.querySelector(".main-frontpage");
 
 mainDom.innerHTML = `
-  <div class="logo-vertical">
-    <img src="./images/egg-logo.png" alt="logo-vertical">
+  <div class="logo">
+    <img src="./images/egg-logo.png" alt="logo">
     <h2>Eggcellent Timer</h1>
   </div>
 `;
 
-const animElement = document.querySelector(".logo-vertical");
+const animElement = document.querySelector(".logo");
 
-animElement.addEventListener("animationend", () =>
-  renderStageOne(headerDom, mainDom)
-);*/
+animElement.addEventListener("animationend", renderStep1);
